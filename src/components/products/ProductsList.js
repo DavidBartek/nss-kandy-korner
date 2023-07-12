8dd54c1a1825cf05e46e487fbc168c65023a0dad
David Bartek <davidwbartek@gmail.com>
2023-07-12 10:09:58 -0500

    Product list sorted alphabetically, now has buttons to filter highest priced products or all

--- src/components/products/ProductsList.js
+++ src/components/products/ProductsList.js
@@ -4,33 +4,65 @@ import "./Products.css"
 export const ProductsList = () => {
     // console.log("welcome to hell")
     const [products, setProducts] = useState([])
+    const [expensiveOnly, setExpensiveOnly] = useState(false)
+    const [filteredProducts, setFiltered] = useState([])
 
     useEffect(
         () => {
             fetch (`http://localhost:8088/products`)
                 .then(res => res.json())
                 .then((productsArray) => {
+                    productsArray.sort((a, b) => {
+                        if (a.name < b.name) {
+                            return -1
+                        } else if ( a.name > b.name ) {
+                            return 1
+                        } else {
+                            return 0
+                        }
+                    })
                     setProducts(productsArray)
                 })
         },
         []
     )
+    
+    useEffect(
+        () => {
+            setFiltered(products)
+        },
+        [products]
+    )
+
+    useEffect(
+        () => {
+            if (expensiveOnly) {
+                const expensiveProducts = products.filter(product => product.unitPrice > 2.00)
+                setFiltered(expensiveProducts)
+            } else {
+                setFiltered(products)
+            }
+        },
+        [expensiveOnly]
+    )
 
     return (
-        <article className="productsList">
+        <>
+            <article className="productsList">
             <h2 className="productsList__title">Product List</h2>
+            <button className="productsButton" onClick={() => { setExpensiveOnly(true) } }>Show Top Priced</button>
+            <button className="productsButton" onClick={() => { setExpensiveOnly(false) } }>Show All</button>
             <div className="productsList__list">
-                {
-                    products.map(
-                        (product) => {
-                            return <section className="productsList__item">
-                                <header>{product.name}</header>
-                                <div>{product.unitPrice}</div>
-                            </section>
-                        }
-                    )
-                }
+                {filteredProducts.map(
+                    (product) => {
+                        return <section className="productsList__item">
+                            <header>{product.name}</header>
+                            <div>{product.unitPrice}</div>
+                        </section>
+                    }
+                )}
             </div>
         </article>
+        </>
     )
 }
